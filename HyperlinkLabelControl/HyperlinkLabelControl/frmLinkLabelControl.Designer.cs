dc144b7b5b796b07a987b721a51f05e10af85682
Luis Vega Torres <lvegat@growthaccelerationpartners.com>
2026-02-03 10:46:24 -0600

    Add HyperlinkLabelControl WinForms project
    Initial import of a new Windows Forms project 'HyperlinkLabelControl'. Adds solution and project files (targets .NET Framework 4.7.2) with DevExpress references, Program.cs entry point, form implementation and designer (frmLinkLabelControl with five HyperlinkLabelControl instances), resources, settings and assembly info. Also includes a .gitignore for common Visual Studio/MSBuild artifacts.

---
+++ HyperlinkLabelControl/HyperlinkLabelControl/frmLinkLabelControl.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace HyperlinkLabelControl
+{
+    partial class frmLinkLabelControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lnkControl1 = new DevExpress.XtraEditors.HyperlinkLabelControl();
+            this.lnkControl2 = new DevExpress.XtraEditors.HyperlinkLabelControl();
+            this.lnkControl3 = new DevExpress.XtraEditors.HyperlinkLabelControl();
+            this.lnkControl4 = new DevExpress.XtraEditors.HyperlinkLabelControl();
+            this.lnkControl5 = new DevExpress.XtraEditors.HyperlinkLabelControl();
+            this.SuspendLayout();
+            // 
+            // lnkControl1
+            // 
+            this.lnkControl1.Location = new System.Drawing.Point(156, 72);
+            this.lnkControl1.Name = "lnkControl1";
+            this.lnkControl1.Size = new System.Drawing.Size(54, 13);
+            this.lnkControl1.TabIndex = 0;
+            this.lnkControl1.Text = "lnkControl1";
+            // 
+            // lnkControl2
+            // 
+            this.lnkControl2.Location = new System.Drawing.Point(156, 112);
+            this.lnkControl2.Name = "lnkControl2";
+            this.lnkControl2.Size = new System.Drawing.Size(54, 13);
+            this.lnkControl2.TabIndex = 1;
+            this.lnkControl2.Text = "lnkControl2";
+            // 
+            // lnkControl3
+            // 
+            this.lnkControl3.Location = new System.Drawing.Point(156, 147);
+            this.lnkControl3.Name = "lnkControl3";
+            this.lnkControl3.Size = new System.Drawing.Size(54, 13);
+            this.lnkControl3.TabIndex = 2;
+            this.lnkControl3.Text = "lnkControl3";
+            // 
+            // lnkControl4
+            // 
+            this.lnkControl4.Location = new System.Drawing.Point(156, 189);
+            this.lnkControl4.Name = "lnkControl4";
+            this.lnkControl4.Size = new System.Drawing.Size(54, 13);
+            this.lnkControl4.TabIndex = 3;
+            this.lnkControl4.Text = "lnkControl4";
+            // 
+            // lnkControl5
+            // 
+            this.lnkControl5.Location = new System.Drawing.Point(156, 230);
+            this.lnkControl5.Name = "lnkControl5";
+            this.lnkControl5.Size = new System.Drawing.Size(54, 13);
+            this.lnkControl5.TabIndex = 4;
+            this.lnkControl5.Text = "lnkControl5";
+            // 
+            // frmLinkLabelControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lnkControl5);
+            this.Controls.Add(this.lnkControl4);
+            this.Controls.Add(this.lnkControl3);
+            this.Controls.Add(this.lnkControl2);
+            this.Controls.Add(this.lnkControl1);
+            this.Name = "frmLinkLabelControl";
+            this.Text = "DevExpress Link Label Control";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.HyperlinkLabelControl lnkControl1;
+        private DevExpress.XtraEditors.HyperlinkLabelControl lnkControl2;
+        private DevExpress.XtraEditors.HyperlinkLabelControl lnkControl3;
+        private DevExpress.XtraEditors.HyperlinkLabelControl lnkControl4;
+        private DevExpress.XtraEditors.HyperlinkLabelControl lnkControl5;
+    }
+}
+
