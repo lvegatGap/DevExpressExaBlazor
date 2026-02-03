7b59856c66506260f0c75766ceae58686783a3c6
Gladys Raquel Bermudez Ureña <glbermudez@growthaccelerationpartners.com>
2026-02-03 13:43:51 -0600

    Adding more labels

--- LabelControl/LabelControl/LabelControl/frnLabelControl.Designer.cs
+++ LabelControl/LabelControl/LabelControl/frnLabelControl.Designer.cs
@@ -28,23 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblLabeControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.lblControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.lblControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.lblControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.lblControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.lblControl5 = new DevExpress.XtraEditors.LabelControl();
             this.SuspendLayout();
             // 
-            // lblLabeControl1
+            // lblControl1
             // 
-            this.lblLabeControl1.Location = new System.Drawing.Point(45, 73);
-            this.lblLabeControl1.Name = "lblLabeControl1";
-            this.lblLabeControl1.Size = new System.Drawing.Size(74, 13);
-            this.lblLabeControl1.TabIndex = 0;
-            this.lblLabeControl1.Text = "lblLabeControl1";
+            this.lblControl1.Location = new System.Drawing.Point(42, 28);
+            this.lblControl1.Name = "lblControl1";
+            this.lblControl1.Size = new System.Drawing.Size(51, 13);
+            this.lblControl1.TabIndex = 0;
+            this.lblControl1.Text = "lblControl1";
+            // 
+            // lblControl2
+            // 
+            this.lblControl2.Location = new System.Drawing.Point(42, 61);
+            this.lblControl2.Name = "lblControl2";
+            this.lblControl2.Size = new System.Drawing.Size(51, 13);
+            this.lblControl2.TabIndex = 1;
+            this.lblControl2.Text = "lblControl2";
+            // 
+            // lblControl3
+            // 
+            this.lblControl3.Location = new System.Drawing.Point(42, 95);
+            this.lblControl3.Name = "lblControl3";
+            this.lblControl3.Size = new System.Drawing.Size(51, 13);
+            this.lblControl3.TabIndex = 2;
+            this.lblControl3.Text = "lblControl3";
+            // 
+            // lblControl4
+            // 
+            this.lblControl4.Location = new System.Drawing.Point(42, 128);
+            this.lblControl4.Name = "lblControl4";
+            this.lblControl4.Size = new System.Drawing.Size(51, 13);
+            this.lblControl4.TabIndex = 3;
+            this.lblControl4.Text = "lblControl4";
+            // 
+            // lblControl5
+            // 
+            this.lblControl5.Location = new System.Drawing.Point(42, 163);
+            this.lblControl5.Name = "lblControl5";
+            this.lblControl5.Size = new System.Drawing.Size(51, 13);
+            this.lblControl5.TabIndex = 4;
+            this.lblControl5.Text = "lblControl5";
             // 
             // frnLabelControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 191);
-            this.Controls.Add(this.lblLabeControl1);
+            this.ClientSize = new System.Drawing.Size(273, 221);
+            this.Controls.Add(this.lblControl5);
+            this.Controls.Add(this.lblControl4);
+            this.Controls.Add(this.lblControl3);
+            this.Controls.Add(this.lblControl2);
+            this.Controls.Add(this.lblControl1);
             this.Name = "frnLabelControl";
             this.Text = "frnLabelControl";
             this.ResumeLayout(false);
@@ -54,7 +94,11 @@
 
         #endregion
 
-        private DevExpress.XtraEditors.LabelControl lblLabeControl1;
+        private DevExpress.XtraEditors.LabelControl lblControl1;
+        private DevExpress.XtraEditors.LabelControl lblControl2;
+        private DevExpress.XtraEditors.LabelControl lblControl3;
+        private DevExpress.XtraEditors.LabelControl lblControl4;
+        private DevExpress.XtraEditors.LabelControl lblControl5;
     }
 }
 
