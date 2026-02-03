b33bf82f6e157428110f8fdf7fb7933ed0f1c40d
Luis Vega Torres <lvegat@growthaccelerationpartners.com>
2026-02-03 13:07:39 -0600

    Add LabelControl WinForms project
    Add a new LabelControl Visual Studio solution and WinForms project targeting .NET Framework 4.7.2. Includes solution (.sln), project file (.csproj) with DevExpress v21.2 references, App.config, Program.cs, AssemblyInfo, autogenerated Resources/Settings designer files, and a scaffolded form (frnLabelControl) with a DevExpress.XtraEditors.LabelControl and associated .Designer.cs/.resx files. This commit provides the initial scaffold to build and run the sample label control form.

---
+++ LabelControl/LabelControl/LabelControl/frnLabelControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace LabelControl
+{
+    partial class frnLabelControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLabeControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // lblLabeControl1
+            // 
+            this.lblLabeControl1.Location = new System.Drawing.Point(45, 73);
+            this.lblLabeControl1.Name = "lblLabeControl1";
+            this.lblLabeControl1.Size = new System.Drawing.Size(74, 13);
+            this.lblLabeControl1.TabIndex = 0;
+            this.lblLabeControl1.Text = "lblLabeControl1";
+            // 
+            // frnLabelControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 191);
+            this.Controls.Add(this.lblLabeControl1);
+            this.Name = "frnLabelControl";
+            this.Text = "frnLabelControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl lblLabeControl1;
+    }
+}
+
